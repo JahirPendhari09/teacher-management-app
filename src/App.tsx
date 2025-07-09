91b0548adcb9392ded52b029a0116297e6500d1b
JahirPendhari09 <jahirpp1999@gmail.com>
2025-07-09 22:57:32 +0530

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='text-red-300'> Hello word
+      </div>
+    </>
+  )
+}
+
+export default App
